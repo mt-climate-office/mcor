4730089f81e4be997d56a6abf5db5da5a1f48cb7
bocinsky <bocinsky@gmail.com>
2018-05-11 06:39:24 -0600

    updated normals

--- R/mco_get_gridmet_normals.R
+++ R/mco_get_gridmet_normals.R
@@ -22,41 +22,67 @@ mco_get_gridmet_normals <-
                         "daily_minimum_temperature",
                         "daily_maximum_temperature"),
            out_dir = tempdir(),
+           p = c(0,
+                 0.025,
+                 0.25,
+                 0.5,
+                 0.75,
+                 0.975,
+                 1),
+           overwrite = FALSE,
            ...)
   {
 
+    if(overwrite)
+      unlink(stringr::str_c(out_dir,"/gridmet_normals.Rds"))
+
     if(!file.exists(stringr::str_c(out_dir,"/gridmet_normals.Rds"))){
+      p.names <- stringr::str_c(p*100, "%")
+
       gridmet <-
         mcor::mco_get_gridmet(dates = c("1981-01-01","2010-12-31"),
                               elements = elements,
                               out_dir = out_dir,
                               overwrite = FALSE,
                               ...) %>%
-        purrr::map(function(x){
+        purrr::map(function(norm){
 
-          x.days <- x %>%
+          the.days <- norm %>%
             names() %>%
             stringr::str_remove("X") %>%
             lubridate::as_date() %>%
             lubridate::yday()
 
-          x %<>%
-            raster::setZ(x.days, name = 'yday')
+          purrr::map(1:365,
+                     function(the.day){
+                       vals <- norm %>%
+                         magrittr::extract2(which(the.days == the.day)) %>%
+                         raster::values()
 
-          x %<>%
-            raster::zApply(by = x.days,
-                           fun = mean)
-
-          gc()
-          gc()
-
-          x[[1:365]]
-
+                       raster::setValues(norm,
+                                         p %>%
+                                           magrittr::set_names(p.names) %>%
+                                           purrr::map(~biwavelet::rcpp_row_quantile(vals,
+                                                                                    q = .x)) %>%
+                                           do.call(cbind, .) %>%
+                                           tibble::as_tibble() %>%
+                                           dplyr::mutate(mean = vals %>%
+                                                           matrixStats::rowMeans2()) %>%
+                                           as.matrix()
+                       ) %>%
+                         raster::as.list() %>%
+                         magrittr::set_names(c(p.names,
+                                               "mean"))
+                     }) %>%
+            purrr::transpose() %>%
+            purrr::map(raster::stack,
+                       quick = TRUE)
         }) %>%
+        # fst::write_fst(stringr::str_c(out_dir,"/gridmet_normals.fst"))
         readr::write_rds(stringr::str_c(out_dir,"/gridmet_normals.Rds"),
-                         compress = "gz")
+                         compress = "bz")
     }
-
+    # fst::read_fst(stringr::str_c(out_dir,"/gridmet_normals.fst"))
     readr::read_rds(stringr::str_c(out_dir,"/gridmet_normals.Rds"))
 
   }

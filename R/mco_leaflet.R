be9012cabb3fd1318403037085b833afea472fa3
Kyle Bocinsky <bocinsky@gmail.com>
2020-05-29 16:04:21 -0600

    updated base map

--- R/mco_leaflet.R
+++ R/mco_leaflet.R
@@ -10,6 +10,7 @@ mco_leaflet <- function(x,
                         midpoint = 0,
                         digits = 1,
                         reverse = FALSE,
+                        pane = "foreground",
                         attribution = "Climate data by <a href='http://www.climatologylab.org/gridmet.html' target='_blank'>gridMET</a> via <a href='https://earthengine.google.com/' target='_blank'>Google Earth Engine</a>",
                         ...){
 
@@ -32,9 +33,11 @@ mco_leaflet <- function(x,
   tm_out$x$calls[[5]]$args[[5]] <- image_query_title
 
   out <- mcor::mco_leaflet_base(attribution = attribution) %>%
-    # leaflet::addRasterImage(x) %>%
-    leafem::addImageQuery(x,
+    leaflet::addRasterImage(x,
+                            group = image_query_title) %>%
+    leafem::addImageQuery(x = x,
                            # type = "click",
+                          group = image_query_title,
                            layerId = image_query_title,
                            prefix = "",
                            digits = digits,
@@ -46,6 +49,7 @@ mco_leaflet <- function(x,
   }
 
 
+  # out$x$calls[[11]]$args[[4]]$pane <- pane
   # tm_out$x$calls[[4]]$args[[4]]$pane <- "background"
   # tm_out$x$calls[[4]]$args[[4]]$attribution <- ""
   # tm_out$x$calls[[4]]$args[[6]] <- ""
@@ -71,23 +75,23 @@ mco_leaflet <- function(x,
       return(x)
     })
 
-  stars <- out$dependencies %>%
-    purrr::keep(~ .x$name == "stars") %>%
-    magrittr::extract2(1) %$%
-    paste0(src$file,"/",script[[1]]) %>%
-    readr::read_file() %>%
-    htmltools::tags$script()
-
-  out$dependencies %<>%
-    purrr::discard(~ .x$name == "stars")
-
-  # out$jsHooks$render <- c(out$jsHooks$render,
-  #                         list(list(code = stars,
-  #                              data = NULL)))
-
-  out %<>%
-    htmlwidgets::appendContent(stars) #%>%
-    # leaflet.opacity::addOpacitySlider(layerId = image_query_title)
+  # stars <- out$dependencies %>%
+  #   purrr::keep(~ .x$name == "stars") %>%
+  #   magrittr::extract2(1) %$%
+  #   paste0(src$file,"/",script[[1]]) %>%
+  #   readr::read_file() %>%
+  #   htmltools::tags$script()
+  #
+  # out$dependencies %<>%
+  #   purrr::discard(~ .x$name == "stars")
+  #
+  # # out$jsHooks$render <- c(out$jsHooks$render,
+  # #                         list(list(code = stars,
+  # #                              data = NULL)))
+  #
+  # out %<>%
+  #   htmlwidgets::appendContent(stars) #%>%
+  #   # leaflet.opacity::addOpacitySlider(layerId = image_query_title)
 
   out
 
@@ -103,18 +107,21 @@ mco_leaflet_base <- function(attribution = ""){
   out <- leaflet::leaflet(options = leaflet::leafletOptions(zoomControl = FALSE)) %>%
     leaflet::addPolygons(data = mcor::mt_state %>%
                            sf::st_transform(4326)) %>%
-    leaflet::addMapPane("background", zIndex = 410) %>%
+    leaflet::addMapPane("background", zIndex = 1) %>%
     leaflet::addMapPane("middleground", zIndex = 420) %>%
     leaflet::addMapPane("foreground", zIndex = 430) %>%
+    leaflet::addProviderTiles("Stamen.TonerBackground",
+                              options = leaflet::providerTileOptions(pane = "background",
+                                                                     attribution = "")) %>%
     leaflet::addTiles("https://api.maptiler.com/tiles/hillshades/{z}/{x}/{y}.png?key=KZO7rAv96Alr8UVUrd4a",
                       attribution = attribution,
                       options = leaflet::tileOptions(pane = "foreground")) %>%
-    leaflet::addProviderTiles("Stamen.TonerLines",
+    leaflet::addProviderTiles("Stamen.TonerHybrid",
                               options = leaflet::providerTileOptions(pane = "foreground",
                                                                      attribution = "map data by <a href='https://www.openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a>, map style by <a href='https://stamen.com/' target='_blank'>Stamen Design</a>")) %>%
-    leaflet::addProviderTiles("Stamen.TonerLabels",
-                              options = leaflet::providerTileOptions(pane = "foreground",
-                                                                     attribution = "")) %>%
+    # leaflet::addProviderTiles("Stamen.TonerLabels",
+    #                           options = leaflet::providerTileOptions(pane = "foreground",
+    #                                                                  attribution = "")) %>%
     leaflet::addScaleBar(position = "bottomright",
                          options = leaflet::scaleBarOptions(metric = FALSE)) %>%
     htmlwidgets::onRender("function(el, x) {L.control.zoom({ position: 'topright' }).addTo(this)}") %>%
